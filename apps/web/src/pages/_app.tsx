3e1acfd72085cd819b1f48264e12616f88b16213
chihab Me <=>
2023-09-12 20:14:14 +0100

    added trpc to nextjs app

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { trpc } from '@/trpc'
 
-export default function App({ Component, pageProps }: AppProps) {
+const  MyApp = ({ Component, pageProps }: AppProps) =>{
   return <Component {...pageProps} />
 }
+export default trpc.withTRPC(MyApp)
+
+
---
+++ apps/web/src/trpc/index.ts
@@ -0,0 +1,50 @@
+
+import {httpBatchLink} from "@trpc/client"
+import {createTRPCNext} from "@trpc/next"
+import type {AppRouter} from "server/src/trpc/router"
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined')
+    // browser should use relative path
+    return '';
+
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+
+  if (process.env.RENDER_INTERNAL_HOSTNAM)
+    // reference for render.com
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+
+  // assume localhost
+   return `http://localhost:${process.SERVER_PORT??2001}`
+}
+export const trpc = createTRPCNext<AppRouter>({
+  config(opts) {
+    // const url = getBaseUrl()+"/api/trpc"
+    const url = "http://localhost:3001/api/trpc"
+    return {
+      links: [
+        httpBatchLink({
+
+          /**
+           * If you want to use SSR, you need to use the server's full URL
+           * @link https://trpc.io/docs/ssr
+           **/
+          url,
+
+          // You can pass any HTTP headers you wish here
+          async headers() {
+            return {
+              // authorization: getAuthCookie(),
+            };
+          },
+        }),
+      ],
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   **/
+  ssr: false,
+});
