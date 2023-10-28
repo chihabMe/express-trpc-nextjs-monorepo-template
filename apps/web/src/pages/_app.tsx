d23094c5e5c46c8a5f73dc3f4febd4da8901a026
chihab <chihab.mg.me@gmail.com>
2023-10-28 19:47:21 +0200

    added prograss bar

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -2,10 +2,12 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { trpc } from "@/trpc";
 import Header from "@/components/layout/Header/Header";
+import NextNProgress from 'nextjs-progressbar';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <NextNProgress color="#3b82f6" />
       <Header />
       <Component {...pageProps} />
     </>
